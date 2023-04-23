4265afa1de0253e57eb67194492009687a4ea22b
Sebastian Whyte <whyte.sebastian14@gmail.com>
2023-04-23 13:37:44 -0400

    Initial commit. Added agent and model files

---
+++ agent.py
@@ -0,0 +1,160 @@
+"""
+This class creates a learning-based agent -- which is used to sense the environment
+through sensors. It then uses an internal mechanism of user feedback, and then make changes according to the performance
+of the action we are taking, which, when aligned with the learning goals, are transformed into the actions which are to be taken
+
+"""
+
+import torch
+import random
+import numpy as np
+from collections import deque
+#from game import SnakeGameAI, Direction, Point  <-- Will need to incorporate the Pydash game class (main.py) to replace this
+from model import Linear_QNet, QTrainer
+#from helper import plot
+
+# Predefined variables
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001              # learning rate
+
+
+class Agent:
+    def __init__(self):
+        self.number_of_games = 0
+        self.epsilon = 0            # for randomness
+        self.gamma = 0.9            # discount rate
+        self.memory = deque(maxlen = MAX_MEMORY)        # holds the actions, pops left once we exceed the max memory capacity
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr = LR, gamma = self.gamma)
+
+    '''
+    Gets the current state of the game
+    
+    Parameters
+    ------------
+    game        instance of our game
+    '''
+    def get_state(self, game):
+
+        # Jump state
+
+        state = []
+
+        # Danger up and right
+
+    '''
+    Associates the old state with the final move, reward, new state, and done, and stores it in the memories
+    
+    Parameters
+    ------------
+    state           state we are currently in
+    action          action for the model to take
+    reward          the reward we get for taking that action
+    next_state      the next state 
+    done            indicates whether we got a game over
+    '''
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, state, done))      # popleft if MAX_MEMORY is reached
+
+
+    '''
+    Trains the model to improve over time
+    '''
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)       # chooses 1000 (BATCH_SIZE) unique random samples from memory, and returns a list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+
+        self.trainer.train_steps(states, actions, rewards, next_states, dones)
+
+        # for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
+
+    '''
+    Trains the model to optimize one step
+    
+    Parameters
+    ------------
+    state           state we are currently in
+    action          action for the model to take
+    reward          the reward we get for taking that action
+    next_state      the next state 
+    done            indicates if we got a game over
+    '''
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_steps(state, action, reward, next_state, done)
+
+
+    '''
+    Random moves: tradeoff exploration / exploitation
+    '''
+    def get_actions(self, state):
+        self.epsilon = 80 - self.number_of_games
+        final_move = [0,0]
+
+        # Get a random integer between 0 and 200
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 1)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype = torch.float)   # creates multi-dimensional matrix containing floats
+            prediction = self.model(state0)                     # model makes a prediction based on the given state
+            move = torch.argmax(prediction).item()              # returns the indices of the maximum value of all elements in the input tensor
+            final_move[move] = 1
+
+        return final_move
+
+
+    '''
+    Training method
+    '''
+    def train(self):
+        plot_scores = []
+        plot_mean_scores = []
+        total_score = 0
+        record = 0
+        agent = Agent()
+        # game = SnakeGameAI()
+
+        while True:
+            state_old = agent.get_state(game)                   # get the old state
+
+            final_move = agent.get_action(state_old)            # get move
+
+            reward, done, score = game.play_step(final_move)    # perform move and get new state
+            state_new = agent.get_state(game)
+
+            agent.train_short_memory(state_old, final_move, reward, state_new, done)    # train short memory
+
+            agent.remember(state_old, final_move, reward, state_new, done)
+
+            if done:
+                # train long memory and plot result
+                game.result()
+                agent.number_of_games += 1
+                agent.train_long_memory()
+
+                # save the best score
+                if score > record:
+                    record = score
+                    # agent.model.save()
+
+                # Print results
+                print('Game', agent.number_of_games, 'Score', score, 'Record', record)
+
+                # update scores
+                plot_scores.append(score)
+                total_score += score
+                mean_score = total_score / agent.number_of_games
+                plot_mean_scores.append(mean_score)
+                plot(plot_scores, plot_mean_scores)
+
+
+# entry point
+if __name__ == '__main__':
+    train()
---
+++ model.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+'''
+'''
+class Linear_QNet(nn.Module):
+    '''
+    Constructor
+
+    Parameters
+    ------------
+    input_size      size of the input we are giving to the model
+    hidden_size     size of the hidden layers
+    output_size     size of the output from our model
+    '''
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
+
+    '''
+    Forward function
+    
+    Parameters
+    -----------
+    
+    '''
+    def forward(self, x):
+        x = F.relu(self.linear1(x))     # first applies a linear transformation to the incoming data (x), then applies the rectified linear unit function element-wise
+        x = self.linear2(x)
+
+        return x
+
+
+    '''
+    
+    Parameters
+    -----------
+    
+    '''
+    def save(self, file_name = 'model.pth'):
+        # Create a folder path for our model
+        model_folder_path = './model'
+
+        # If the path doesn't exist, make a new directory for it
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        # Join the path and the filename
+        file_name = os.path.join(model_folder_path, file_name)
+
+        # Create a dictionary and map each layer to its parameter tensor. Then save it to the disk file
+        torch.save(self.state_dict(), file_name)
+
+
+
+class QTrainer:
+    '''
+    Constructor
+
+    Parameters
+    ------------
+    model       instance of our model
+    lr          learning rate
+    gamma
+    '''
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr = self.lr)
+
+
+    '''
+    Trains the next step for the model to take
+    
+    Parameters
+    -----------
+    state           state we are currently in
+    action          action for the model to take
+    reward          the reward we get for taking that action
+    next_state      the next state 
+    done            indicates whether we got a game over
+    '''
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype = torch.float)
+        next_state = torch.tensor(next_state, dtype = torch.float)
+        action = torch.tensor(action, dtype = torch.float)
+        reward = torch.tensor(reward, dtype = torch.float)
+        # (n, x)
+
+        # The shape of the state should be 1
+        if len(state.shape) == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+            #1 : predicted Q values with current state
+            pred = self.model(state)
+
+            target = pred.clone()
+
+            for index in range(len(done)):
+                Q_new = reward[index]
+
+                if not done[index]:
+                    Q_new = reward[index] + self.gamma * torch.max(self.model(next_state[index]))
+
+                target[index][torch.argmax(action[index]).item()] = Q_new
+
+            #2 : Q_new = r + y * max(next_predicted Q value) ->  only do this if not done
+            # pred.clone()
+            # preds[argmax(action)] = Q_new
+            self.optimizer.zero_grad()
+            loss = self.criterion(target, pred)
+            loss.backward()
+
+            self.optimizer.step()
